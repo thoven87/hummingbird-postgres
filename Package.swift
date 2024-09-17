a7c1ea77813b34994c7aab53927edb76b509c503
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2024-09-16 22:49:16 -0400

    load testing

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
-        .package(url: "https://github.com/hummingbird-project/swift-jobs.git", branch: "main"),
+        .package(url: "https://github.com/thoven87/swift-jobs.git", branch: "stevenson/metrics-bug-fix"),
         .package(url: "https://github.com/vapor/postgres-nio", from: "1.21.0"),
     ],
     targets: [
